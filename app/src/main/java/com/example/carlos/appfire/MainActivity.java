5a2e974b18690a7d723bd14a658e26ca63cc4e94
Carlos <chvs@cin.ufpe.br>
2018-10-03 12:32:10 -0300

    Teste

--- app/src/main/java/com/example/carlos/appfire/MainActivity.java
+++ app/src/main/java/com/example/carlos/appfire/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.carlos.appfire;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -20,6 +29,9 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String CHANNEL_ID = "personal_notifications";
+    private final int mNotificationId= 001;
+
     private GifImageView gifImageView;
     private ProgressBar progressBar;
     private String gif;
@@ -29,11 +41,32 @@ public class MainActivity extends AppCompatActivity {
     //Método com as trocas de gifs e mensagem
     private void changeGif (int tempo){
 
+        String titulo;
+        String texto;
+
+        //Mais Teste
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+
         switch (tempo){
             case 4:
 
                 gif = "Fire3hp.gif";
                 try{
+                    titulo = "Cuide da chama!";
+                    texto = "A chama estar alta, mantenha ela assim!";
+
+                    //Teste metodo
+                    mBuilder = mBuilder.setContentTitle(titulo);
+                    mBuilder = mBuilder.setContentText(texto);
+                    mBuilder = mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+                    mBuilder = mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+                    notificationManager.notify(mNotificationId, mBuilder.build());
+                    //Fim teste metodo
+
                     InputStream inputStream = getAssets().open(gif);
                     byte[] bytes = IOUtils.toByteArray(inputStream);
                     gifImageView.setBytes(bytes);
@@ -48,6 +81,21 @@ public class MainActivity extends AppCompatActivity {
 
                 gif = "Fire2hp.gif";
                 try{
+                    titulo = "Cuide da chama 2!";
+                    texto = "A chama estar se apagando, mantenha a chama alta!";
+
+                    //Teste metodo
+                    mBuilder = mBuilder.setContentTitle(titulo);
+                    mBuilder = mBuilder.setContentText(texto);
+                    mBuilder = mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+                    mBuilder = mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+                    notificationManager.notify(mNotificationId, mBuilder.build());
+                    //Fim teste metodo
+
                     InputStream inputStream = getAssets().open(gif);
                     byte[] bytes = IOUtils.toByteArray(inputStream);
                     gifImageView.setBytes(bytes);
@@ -62,6 +110,22 @@ public class MainActivity extends AppCompatActivity {
 
                 gif = "Fire1hp.gif";
                 try{
+                    titulo = "Cuide da chama 3!";
+                    texto = "A chama estar quase no fim, não deixe ela apagar!";
+
+                    //Teste metodo
+                    mBuilder = mBuilder.setContentTitle(titulo);
+                    mBuilder = mBuilder.setContentText(texto);
+                    mBuilder = mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+                    mBuilder = mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+                    notificationManager.notify(mNotificationId, mBuilder.build());
+                    //Fim teste metodo
+
+
                     InputStream inputStream = getAssets().open(gif);
                     byte[] bytes = IOUtils.toByteArray(inputStream);
                     gifImageView.setBytes(bytes);
@@ -74,8 +138,24 @@ public class MainActivity extends AppCompatActivity {
 
             default:
 
-                gif = "fire.gif";
+                gif = "Fire0hp.gif";
                 try{
+                    titulo = "Cuide da chama 4!";
+                    texto = "A chama se apagou :(";
+
+                    //Teste metodo
+                    mBuilder = mBuilder.setContentTitle(titulo);
+                    mBuilder = mBuilder.setContentText(texto);
+                    mBuilder = mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+                    mBuilder = mBuilder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);;
+
+                    notificationManager.notify(mNotificationId, mBuilder.build());
+                    //Fim teste metodo
+
+
                     InputStream inputStream = getAssets().open(gif);
                     byte[] bytes = IOUtils.toByteArray(inputStream);
                     gifImageView.setBytes(bytes);
@@ -105,6 +185,9 @@ public class MainActivity extends AppCompatActivity {
         tempo = 4;
         changeGif(tempo);
 
+        //Channel para notificações das versões 27+
+        createNotificationChannel();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,8 +206,8 @@ public class MainActivity extends AppCompatActivity {
     private Runnable runnableCode = new Runnable() {
         @Override
         public void run() {
-            tempo--;
             changeGif(tempo);
+            tempo--;
             // Tempo de repetição
             handler.postDelayed(runnableCode, 60000);
         }
@@ -151,4 +234,24 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void createNotificationChannel(){
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+
+            CharSequence name = "Personal Notification";
+            String description = "Include all the personal notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel notificationChannel = new NotificationChannel (CHANNEL_ID, name, importance);
+
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+
+    }
+
 }
